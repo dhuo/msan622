3da0d224a24ae6f65d1d3a89ca055e1aaad51506
dhuo <dhuo@dons.usfca.edu>
2014-05-13 11:27:00 -0700

    other

--- project-prototype/server.R
+++ project-prototype/server.R
@@ -2,25 +2,29 @@ library(ggplot2)
 library(shiny)
 library(grid)
 library(scales)
+library(reshape)
 
 
 
 loadData <- function(){
-  mydata <- read.csv("index2014_data.csv" ,  ',' , header=T)
-
+  mydata <- read.table("index2014_data1.csv",',', header=T,stringsAsFactors = F)
 
   return (mydata)
 }
 
 
-BubblePlot<- function(economic_data){
-  plot <- data.frame(economic_data)
+
+# bubble plot
+BubblePlot<- function(data){
+  plot <- data.frame(data)
   #Creat base plot
-  p <- ggplot(plot, aes(x = GDP.Billions.ppp,y = Freedom.from.Corruption,color = as.factor(Region),size = Population.Millions))
-  p <- p + geom_point (alpha=0.6, position = 'jitter') + labs(color='Region')+ labs(size='World rank')
-  anontateText<-paste("Circle area is proportional to World Rank")
-  p <- p + annotate("text", x = 30, y = 4,hjust = 0.5,alpha=0.6, color = "grey40", size=4,label = anontateText)
+  p <- ggplot(plot, aes(x = as.numeric(GDPcapita),y = as.numeric(FreedomFCorruption),color = Region))
+  p <- p + geom_point (alpha=0.6, position = 'jitter') + labs(color='Region')+xlab("GDP Per Capita")+ ylab("Freedom From Corruption Score out of 100")
+  p <- p + geom_text(aes(label=Name,color=Region), hjust= 0.5,vjust=0)
+  anontateText<-paste("")
+  p <- p + annotate("text", x = 500, y = 4,hjust = 0.5,alpha=0.6, color = "grey40", size=4,label = anontateText)
   p <- p+ scale_size_discrete(guide ='none')
+ 
   p<- p + theme(panel.background = element_rect(fill = NA))
   p <- p+ theme(panel.grid.major = element_blank())
   p <- p + theme(panel.grid.minor = element_blank())
@@ -38,25 +42,40 @@ BubblePlot<- function(economic_data){
   return (p)
 }
 
+#Smalle Multiples
+multiPlot <- function(data){
+  plot1<- data.frame(data)
+  p1 <- ggplot(plot1, aes(x=as.numeric(unemp.),y = as.numeic(inflation.),
+                          color= Region))
+  
+  return(p1)
+}
+  
 
 
 
 globalData<-loadData()
 
+
 shinyServer(function(input, output){
-  
+
   cat("Press \"ESC\" to exit...\n")
-  economic_data <- globalData
+ data<- globalData
   
   
   
   output$plot1 <- renderPlot({
     plot1 <- BubblePlot(
-      economic_data
+      data
     )
     print(plot1)
   })
   
+ output$multiPlot<- renderPlot({
+   multiPlot<- multiPlot(
+     data)
+   print(multiPlot)
+ })
 
   
   
--- project-prototype/ui.R
+++ project-prototype/ui.R
@@ -1,25 +1,40 @@
 library (shiny)
 
 
-shinyUI(
-  navbarPage('Countries Comparison Using Economic Indicators',
-             tabPanel('Bubble Plot',
-                      
+shinyUI(     
+  navbarPage("Countries Comparison Using Economic Indicators",
+            tabPanel("Scatter Plot",
                       pageWithSidebar(
-                        headerPanel('Bubble_Plot'),
-                        sidebarPanel(  
-                          selectInput('SizeUI', 'Bubble Size Defined By:', choices=c('GDP.Billions.ppp','World.Rank','Fiscal.Freedom','Freedom.from.Corruption',
-                                                                             'Business.Freedom','Labor.Freedom','Monetary.Freedom')),
-                          selectInput('xUI', 'X:', choices=c('GDP.Billions.ppp','World.Rank','Fiscal.Freedom','Freedom.from.Corruption',
+                        headerPanel("Freemdom from Corruption vs. GDP Capita"),
+                        sidebarPanel( 
+                          selectInput('xUI', 'X-Axis Choice:', choices=c('GDPcapita','World.Rank','Fiscal.Freedom','FreedomFCorruption',
                                                                   'Business.Freedom','Labor.Freedom','Monetary.Freedom')),
-                          selectInput('yUI', 'Y:', choices=c('GDP.Billions.ppp','World.Rank','Fiscal.Freedom','Freedom.from.Corruption',
-                                                                  'Business.Freedom','Labor.Freedom','Monetary.Freedom'))
+                          selectInput('yUI', 'Y-Axis Choice:', choices=c('FreedomFCorruption','World.Rank','Fiscal.Freedom','GDPcapita',
+                                                                  'Business.Freedom','Labor.Freedom','Monetary.Freedom')),
+                          sliderInput("xlimUI","GDP Range:", min=0, max=102900, value=c(0.100))
                         ),
                        
                         mainPanel(  
-                          tabPanel('Bubble_Plot', plotOutput('plot1'), width='200%', height='1000px'))
-                      ))
+                          tabPanel("Scatter Plot",
+                                   plotOutput('plot1', width='90%', height='1000px'))
+                          )
+                      )
+                      ),
+            tabPanel("Multiple Plot",
+                     sidebarPanel(
+                       selectInput("MultiUI", "MultiPlot Choice :", choices=c('FreedomFCorruption','World.Rank','Fiscal.Freedom','GDPcapita',
+                                                                  'Business.Freedom','Labor.Freedom','Monetary.Freedom'))
+                       ),
+                     mainPanel("Scatter Plot Matrix", plotOutput("multiPlot", width = "100%",height = "1000px" ))
+                     
+                       
+
+),
+           tabPanel("Parallel Coordinate Plot"),
+           tabPanel("Bar Plot")
+))
+
              
-                     )
+
+
              
-  )
